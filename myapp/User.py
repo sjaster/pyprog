f52ed9f667da8d29de599b73d2f53a1acfdaa6d0
nicco <nicco.borgioli@gmail.com>
2017-12-20 14:36:18 +0100

    Lesson

---
+++ myapp/User.py
@@ -0,0 +1,5 @@
+class User:
+
+    def __init__(self, new_name, new_password):
+        self.name = new_name
+        self.password = new_password
--- myapp/app.py
+++ myapp/app.py
@@ -1,25 +1,39 @@
 from flask import Flask, session, redirect, url_for, escape, request, render_template
+from User import User
+
+users = []                              # Users List
+
+admin = User('admin', 'haxxor')      # User Object
+users.append(admin)                  # Adding user object to list
+users.append(User('test', '123'))    # Adding another uyser
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',  methods=['GET'])
 def index():
     return render_template('index.html', name=session.get('username', ''))
 
 
 @app.route('/login', methods=['POST'])
 def login_action():
-    session['username'] = request.form['username']
-    return redirect(url_for('index'))
+    myUser = request.form['username']
+    myPass = request.form['password']
+
+    for current_user in users:
+        if myUser == current_user.name and myPass == current_user.password:
+            session['username'] = myUser
+            return redirect(url_for('index'))
+
+    return redirect(url_for('login', error='Wrong user or password'))
 
 
 @app.route('/login', methods=['GET'])
 def login():
-    return render_template('login.html')
+    return render_template('login.html', error=request.args.get('error', None))
 
 
-@app.route('/logout')
+@app.route('/logout',  methods=['GET'])
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
